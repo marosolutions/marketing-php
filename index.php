e6a98c428be61a0e0ae8631133975e3b15b5b59b
dlamb <damb@ap-logic.com>
2019-02-15 14:18:58 -0800

    + Journeys API

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require('autoloader.php');
 
 $reports = new Maropost\Api\Reports(1000, 'wTX-esFcYzEMjSLEqkdWgGcf8yR7osiROc9uU-CjJXQDxMshn_SM-Q');
 $campaigns = new Maropost\Api\AbTestCampaigns(1000, 'wTX-esFcYzEMjSLEqkdWgGcf8yR7osiROc9uU-CjJXQDxMshn_SM-Q');
+$journeys = new Maropost\Api\Journeys(1000, 'wTX-esFcYzEMjSLEqkdWgGcf8yR7osiROc9uU-CjJXQDxMshn_SM-Q');
 
 // gets Opens with all api documented examples
 $res = $reports->getOpens(['fields' => 'email,first_name,city,last_name', 'from' => '2016-06-28', 'to' => '2019-04-11', 'unique' => true, 'per' => 3]);
@@ -27,4 +28,12 @@ var_dump($res->getData());
 
 // gets Journeys with all api documented examples
 $res = $reports->getAbReports(['fields' => 'email,first_name,city,last_name', 'from' => '2016-06-28', 'to' => '2019-04-11', 'unique' => true, 'per' => 3]);
+var_dump($res->getData());
+
+// gets Journeys via Journeys api
+$res = $journeys->get();
+var_dump($res->getData());
+
+// gets Journey campaigns
+$res = $journeys->getCampaigns(357);
 var_dump($res->getData());
