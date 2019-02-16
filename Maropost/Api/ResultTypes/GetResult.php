1dfb3ae1c062c43d416d33b2e28eb32d684fb486
dlamb <damb@ap-logic.com>
2019-02-16 15:50:36 -0800

    fix/cleanup error message logic

--- Maropost/Api/ResultTypes/GetResult.php
+++ Maropost/Api/ResultTypes/GetResult.php
@@ -20,27 +20,33 @@ class GetResult extends OperationResult
         if (is_null($apiResponse)) {
             $this->isSuccess = false;
             $this->errorMessage = $errorMessage;
-            return;
-        }
-        elseif ($apiResponse->code >= 200 && $apiResponse->code < 300) {
-            $this->data = $apiResponse->body;
-            $this->errorMessage = !empty($this->errorMessage) ? $this->errorMessage : isset($apiResponse->body->error) ? $apiResponse->body->error : '';
-            $this->isSuccess = (strlen($this->errorMessage) == 0);
-            return;
-        }
-        $this->isSuccess = false;
-        $this->data = $apiResponse->body;
-        if ($apiResponse->code >= 500) {
-            $this->errorMessage = $apiResponse->code.": Maropost experienced a server error and could not complete your request.";
-        }
-        elseif ($apiResponse->code >= 400) {
-            $this->errorMessage = $apiResponse->code.": Either your accountId, authToken, or one (or more) of your function arguments are invalid.";
-        }
-        elseif ($apiResponse->code >= 300) {
-            $this->errorMessage = $apiResponse->code.": This Maropost API function is currently unavailable.";
         }
         else {
-            $this->errorMessage = $apiResponse->code.": Unexpected final response from Maropost.";
+            $this->data = $apiResponse->body;
+            $this->errorMessage = !empty($this->errorMessage) ? $this->errorMessage : isset($apiResponse->body->error) ? $apiResponse->body->error : '';
+            if ($apiResponse->code >= 200 && $apiResponse->code < 300) {
+                $this->isSuccess = (strlen($this->errorMessage) == 0);
+            }
+            else {
+                $this->isSuccess = false;
+                if (empty($this->errorMessage)) {
+                    if (isset($apiResponse->body->message) && !empty($apiResponse->body->message)) {
+                        $this->errorMessage = $apiResponse->body->message;
+                    }
+                    elseif ($apiResponse->code >= 500) {
+                        $this->errorMessage = $apiResponse->code.": Maropost experienced a server error and could not complete your request.";
+                    }
+                    elseif ($apiResponse->code >= 400) {
+                        $this->errorMessage = $apiResponse->code.": Either your accountId, authToken, or one (or more) of your function arguments are invalid.";
+                    }
+                    elseif ($apiResponse->code >= 300) {
+                        $this->errorMessage = $apiResponse->code.": This Maropost API function is currently unavailable.";
+                    }
+                    else {
+                        $this->errorMessage = $apiResponse->code.": Unexpected final response from Maropost.";
+                    }
+                }
+            }
         }
     }
 }
