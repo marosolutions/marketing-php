ac80350190eadfdc7b927a9a06c60770a41f1a3d
dlamb <damb@ap-logic.com>
2019-02-22 18:45:16 -0800

    RelationalTables unit tests

--- tests/RelationalTablesTests.php
+++ tests/RelationalTablesTests.php
@@ -10,16 +10,23 @@ use \Maropost\Api\RelationalTables;
 final class RelationalTablesTests extends TestCase
 {
     const ACCOUNT_ID = 1000;
-    const AUTH_TOKEN = "wTX-esFcYzEMjSLEqkdWgGcf8yR7osiROc9uU-CjJXQDxMshn_SM-Q";
-    const DEFAULT_TABLENAME = "naveen_one";
-    const DEFAULT_SHOW_FIELDNAME = "ID";
-    const DEFAULT_SHOW_FIELDVALUE = "1121";
+    const AUTH_TOKEN = "";
+    const DEFAULT_TABLENAME = "phpunit_testing_for_api";
     const TEST_TABLE_NAME = "phpunit_testing_for_api";
     const TEST_FIELDNAME = "email";
-    const TEST_FIELDVALUE = "dlamb@aplogic.com";
+    const TEST_FIELDVALUE = "asdfg@maropost.com";
 
-    const INVALID_ACCOUNT_ID = 1000;
     const INVALID_AUTH_TOKEN = "asdf";
+
+    /**
+     * @return array - associative array. First record contains sample primary key
+     */
+    private function getKeyValues() : array
+    {
+        return array(new KeyValue(self::TEST_FIELDNAME, self::TEST_FIELDVALUE),
+            new KeyValue("firstName", "Aaaaa"),
+            new KeyValue("lastName", "Bbbbb"));
+    }
 
     public function testInit()
     {
@@ -27,7 +34,7 @@ final class RelationalTablesTests extends TestCase
         $this->assertInstanceOf(RelationalTables::class, $svc);
     }
 
-    public function testGetAndSetTableName()
+    public function testGetTableNameAndSetTableName()
     {
         $svc = RelationalTables::init(self::ACCOUNT_ID, self::AUTH_TOKEN, self::DEFAULT_TABLENAME);
         $this->assertEquals(self::DEFAULT_TABLENAME, $svc->_getTableName(self::DEFAULT_TABLENAME));
@@ -50,13 +57,13 @@ final class RelationalTablesTests extends TestCase
         $records = $data->records;
         $this->assertTrue(is_array($records));
         $this->assertFalse(count($records) == 0);
-        $this->assertObjectHasAttribute("email", $records[0]);
+        $this->assertObjectHasAttribute(self::TEST_FIELDNAME, $records[0]);
     }
 
     public function testShow()
     {
         $svc = RelationalTables::init(self::ACCOUNT_ID, self::AUTH_TOKEN, self::DEFAULT_TABLENAME);
-        $results = $svc->show(self::DEFAULT_SHOW_FIELDNAME, self::DEFAULT_SHOW_FIELDVALUE);
+        $results = $svc->show(self::TEST_FIELDNAME, self::TEST_FIELDVALUE);
         $this->assertInstanceOf(OperationResult::class, $results);
         $this->assertTrue($results->isSuccess);
         $this->assertEmpty($results->errorMessage);
@@ -71,9 +78,7 @@ final class RelationalTablesTests extends TestCase
         $results = $svc->show(self::TEST_FIELDNAME, self::TEST_FIELDVALUE);
         if($results->isSuccess && property_exists($results->getData()->result, "error")) {
             // the record doesn't exist yet; we need to insert one before we can delete it.
-            $keyValues = array(new KeyValue(self::TEST_FIELDNAME, self::TEST_FIELDVALUE),
-                new KeyValue("firstName", "David"),
-                new KeyValue("lastName", "Lamb"));
+            $keyValues = $this->getKeyValues();
             $svc->create($keyValues[0], $keyValues[1], $keyValues[2]);
         }
         $results = $svc->delete(self::TEST_FIELDNAME, self::TEST_FIELDVALUE);
@@ -93,9 +98,7 @@ final class RelationalTablesTests extends TestCase
             // we need to delete the row before we can insert it.
             $svc->delete(self::TEST_FIELDNAME, self::TEST_FIELDVALUE);
         }
-        $keyValues = array(new KeyValue(self::TEST_FIELDNAME, self::TEST_FIELDVALUE),
-            new KeyValue("firstName", "David"),
-            new KeyValue("lastName", "Lamb"));
+        $keyValues = $this->getKeyValues();
         $results = $svc->create($keyValues[0], $keyValues[1], $keyValues[2]);
         $this->assertInstanceOf(OperationResult::class, $results);
         $this->assertTrue($results->isSuccess);
@@ -105,6 +108,56 @@ final class RelationalTablesTests extends TestCase
         $this->assertObjectHasAttribute("result", $data);
         $result = $data->result;
         $this->assertEquals(1, $result->created);
+    }
+
+    public function testUpsert()
+    {
+        $svc = RelationalTables::init(self::ACCOUNT_ID, self::AUTH_TOKEN, self::TEST_TABLE_NAME);
+        $keyValues = $this->getKeyValues();
+
+        //$results = $svc->show(self::TEST_FIELDNAME, self::TEST_FIELDVALUE);
+        //$result = $results->getData()->result;
+        //if (property_exists($result, "record") && property_exists($result->record, self::TEST_FIELDNAME))
+        //{
+            // we need to delete potential row before we can insert it.
+            $svc->delete($keyValues[0]->key, $keyValues[0]->value);
+        //}
+
+        // test insert via upsert.
+        $results = $svc->upsert($keyValues[0], $keyValues[1], $keyValues[2]);
+        $this->assertInstanceOf(OperationResult::class, $results);
+        $this->assertTrue($results->isSuccess);
+        $this->assertEmpty($results->errorMessage);
+        $this->assertNull($results->exception);
+        $data = $results->getData();
+        $this->assertObjectHasAttribute("result", $data);
+        $result = $data->result;
+        $this->assertObjectHasAttribute("created", $result);
+        $this->assertEquals(1, $result->created);
+
+        // test update via upsert.
+        $lastIndex = count($keyValues) - 1;
+        $origFieldKeyValue = $keyValues[$lastIndex];
+        $newFieldValue = $origFieldKeyValue->value . "diff";
+        $keyValues[$lastIndex]->value = $newFieldValue;
+        $results = $svc->upsert($keyValues[0], $keyValues[1], $keyValues[2]);
+        $this->assertInstanceOf(OperationResult::class, $results);
+        $this->assertTrue($results->isSuccess);
+        $this->assertEmpty($results->errorMessage);
+        $this->assertNull($results->exception);
+        $data = $results->getData();
+        $this->assertObjectHasAttribute("result", $data);
+        $result = $data->result;
+        $this->assertObjectHasAttribute("updated", $result);
+        $this->assertEquals(1, $result->updated);
+
+        // test missing primary key fails.
+        $results = $svc->upsert($keyValues[1], $keyValues[2]);
+        $this->assertInstanceOf(OperationResult::class, $results);
+        $this->assertFalse($results->isSuccess);
+
+        // undo new record.
+        $svc->delete(self::TEST_FIELDNAME, self::TEST_FIELDVALUE);
     }
 
 }
