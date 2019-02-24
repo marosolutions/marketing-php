ccb0a82eea8ea68c0cbf2c0f8ac00d16993984c3
dlamb <damb@ap-logic.com>
2019-02-24 11:26:36 -0800

    ProductAndRevenue unit tests

--- tests/ProductAndRevenueTests.php
+++ tests/ProductAndRevenueTests.php
@@ -20,87 +20,6 @@ final class ProductAndRevenueTests extends TestCase
         );
     }
 
-    public function testGetOrder()
-    {
-        // order 233
-        $svc = new ProductAndRevenue(self::ACCOUNT_ID, self::AUTH_TOKEN);
-        $results = $svc->getOrder(233);
-        $this->assertInstanceOf(OperationResult::class, $results);
-        $this->assertTrue($results->isSuccess);
-        $this->assertEmpty($results->errorMessage);
-        $this->assertNull($results->exception);
-        $data = $results->getData();
-        $this->assertNotEmpty($data);
-        $this->assertObjectHasAttribute("id", $data);
-        $id = $data->id;
-
-        // order 234
-        $results = $svc->getOrder(234);
-        $this->assertInstanceOf(OperationResult::class, $results);
-        $this->assertTrue($results->isSuccess);
-        $this->assertEmpty($results->errorMessage);
-        $this->assertNull($results->exception);
-        $data = $results->getData();
-        $this->assertNotEmpty($data);
-        $this->assertObjectHasAttribute("id", $data);
-        $id2 = $data->id;
-
-        $this->assertNotEquals($id, $id2, "first order (". $id . ") should not equal second order (". $id2 . ").");
-    }
-
-    public function testGetOrderForOriginalOrderId()
-    {
-        // order 233
-        $svc = new ProductAndRevenue(self::ACCOUNT_ID, self::AUTH_TOKEN);
-        $results = $svc->getOrderForOriginalOrderId("PhpUnit_2017-02-23-00-29-09-00000");
-        $this->assertInstanceOf(OperationResult::class, $results);
-        $this->assertTrue($results->isSuccess);
-        $this->assertEmpty($results->errorMessage);
-        $this->assertNull($results->exception);
-        $data = $results->getData();
-        $this->assertNotEmpty($data);
-        $this->assertObjectHasAttribute("id", $data);
-        $id = $data->id;
-
-        // order 234
-        $results = $svc->getOrderForOriginalOrderId("PhpUnit_2017-02-23-00-29-10-00000");
-        $this->assertInstanceOf(OperationResult::class, $results);
-        $this->assertTrue($results->isSuccess);
-        $this->assertEmpty($results->errorMessage);
-        $this->assertNull($results->exception);
-        $data = $results->getData();
-        $this->assertNotEmpty($data);
-        $this->assertObjectHasAttribute("id", $data);
-        $id2 = $data->id;
-
-        $this->assertNotEquals($id, $id2, "first order (". $id . ") should not equal second order (". $id2 . ").");
-    }
-
-    public function testCreateOrder()
-    {
-        $svc = new ProductAndRevenue(self::ACCOUNT_ID, self::AUTH_TOKEN);
-        $rootOrigOrderId = "phpUnit_";
-        $originalOrderId = $rootOrigOrderId;
-        try {
-            $originalOrderId = $originalOrderId . (new DateTime())->format('Y-m-d-H:i:s:u');
-        }
-        catch (Exception $e) {
-            $this->assertNull($e);
-        }
-        $orderItems = $this->getTestOrderItems();
-        $results = $svc->createOrder(true, "info@maropost.com", "TestFirstName",
-            "TestLastName", "2017-10-13T18:05:24-04:00", "Processed", $originalOrderId,
-            $orderItems);
-        $this->assertInstanceOf(OperationResult::class, $results);
-        $this->assertTrue($results->isSuccess);
-        $this->assertEmpty($results->errorMessage);
-        $this->assertNull($results->exception);
-        $data = $results->getData();
-        $this->assertNotEmpty($data);
-        $this->assertObjectHasAttribute("id", $data);
-        $svc->deleteForOriginalOrderId($originalOrderId);
-    }
-
     /**
      * Creates a test order. Try to delete this order after you finish testing.
      *
@@ -128,6 +47,63 @@ final class ProductAndRevenueTests extends TestCase
         return "";
     }
 
+    public function testGetOrder()
+    {
+        $svc = new ProductAndRevenue(self::ACCOUNT_ID, self::AUTH_TOKEN);
+        $originalOrderId = $this->createTestOrder($svc);
+        $results = $svc->getOrderForOriginalOrderId($originalOrderId);
+        $orderId = $results->getData()->id;
+        $results = $svc->getOrder($orderId);
+        $this->assertInstanceOf(OperationResult::class, $results);
+        $this->assertTrue($results->isSuccess);
+        $this->assertEmpty($results->errorMessage);
+        $this->assertNull($results->exception);
+        $data = $results->getData();
+        $this->assertNotEmpty($data);
+        $this->assertObjectHasAttribute("id", $data);
+        $svc->deleteForOriginalOrderId($originalOrderId);
+    }
+
+    public function testGetOrderForOriginalOrderId()
+    {
+        $svc = new ProductAndRevenue(self::ACCOUNT_ID, self::AUTH_TOKEN);
+        $originalOrderId = $this->createTestOrder($svc);
+        $results = $svc->getOrderForOriginalOrderId($originalOrderId);
+        $this->assertInstanceOf(OperationResult::class, $results);
+        $this->assertTrue($results->isSuccess);
+        $this->assertEmpty($results->errorMessage);
+        $this->assertNull($results->exception);
+        $data = $results->getData();
+        $this->assertNotEmpty($data);
+        $this->assertObjectHasAttribute("id", $data);
+        $svc->deleteForOriginalOrderId($originalOrderId);
+    }
+
+    public function testCreateOrder()
+    {
+        $svc = new ProductAndRevenue(self::ACCOUNT_ID, self::AUTH_TOKEN);
+        $rootOrigOrderId = "phpUnit_";
+        $originalOrderId = $rootOrigOrderId;
+        try {
+            $originalOrderId = $originalOrderId . (new DateTime())->format('Y-m-d-H:i:s:u');
+        }
+        catch (Exception $e) {
+            $this->assertNull($e);
+        }
+        $orderItems = $this->getTestOrderItems();
+        $results = $svc->createOrder(true, "info@maropost.com", "TestFirstName",
+            "TestLastName", "2017-10-13T18:05:24-04:00", "Processed", $originalOrderId,
+            $orderItems);
+        $this->assertInstanceOf(OperationResult::class, $results);
+        $this->assertTrue($results->isSuccess);
+        $this->assertEmpty($results->errorMessage);
+        $this->assertNull($results->exception);
+        $data = $results->getData();
+        $this->assertNotEmpty($data);
+        $this->assertObjectHasAttribute("id", $data);
+        $svc->deleteForOriginalOrderId($originalOrderId);
+    }
+
     public function testUpdateOrderForOrderId()
     {
         // create sample order and update it.
@@ -150,7 +126,7 @@ final class ProductAndRevenueTests extends TestCase
         $this->assertEmpty($results->errorMessage);
         $this->assertNull($results->exception);
 
-        // re-pull the order to confirm it's changed, and then delete.
+        // re-pull the order to confirm it's changed, and then delete. Failure here indicates likely bugs in REST API itself.
         $results = $svc->getOrderForOriginalOrderId($originalOrderId);
         $orderData = $results->getData();
         //$this->assertEquals("2018-01-01T15:00:00-07:00", $orderData->order_date); // TODO: restore this test after the REST API itself is fixed.
@@ -177,7 +153,7 @@ final class ProductAndRevenueTests extends TestCase
         $this->assertEmpty($results->errorMessage);
         $this->assertNull($results->exception);
 
-        // re-pull the order to confirm it's changed, and then delete.
+        // re-pull the order to confirm it's changed, and then delete. Failure here indicates likely bugs in REST API itself.
         $results = $svc->getOrderForOriginalOrderId($originalOrderId);
         $orderData = $results->getData();
         //$this->assertEquals("2018-01-01T15:00:00-07:00", $orderData->order_date); // TODO: restore this test after the REST API itself is fixed.
@@ -188,6 +164,89 @@ final class ProductAndRevenueTests extends TestCase
         $this->assertEquals("7", $orderItems[0]->quantity);
         $svc->deleteForOriginalOrderId($originalOrderId);
     }
-    /*
-     */
+
+    public function testDeleteForOriginalOrderId()
+    {
+        $svc = new ProductAndRevenue(self::ACCOUNT_ID, self::AUTH_TOKEN);
+        $originalOrderId = $this->createTestOrder($svc);
+        $results = $svc->getOrderForOriginalOrderId($originalOrderId);
+        $this->assertTrue($results->isSuccess);
+        $results = $svc->deleteForOriginalOrderId($originalOrderId);
+        $this->assertTrue($results->isSuccess);
+
+        // confirm deleted order is gone.
+        $results = $svc->getOrderForOriginalOrderId($originalOrderId);
+        $this->assertFalse($results->isSuccess);
+
+        // also confirm attempting to get non-existent order fails.
+        $results = $svc->deleteForOriginalOrderId($originalOrderId);
+        $this->assertFalse($results->isSuccess);
+    }
+
+    public function testDeleteForOrderId()
+    {
+        $svc = new ProductAndRevenue(self::ACCOUNT_ID, self::AUTH_TOKEN);
+        $originalOrderId = $this->createTestOrder($svc);
+        $results = $svc->getOrderForOriginalOrderId($originalOrderId);
+        $this->assertTrue($results->isSuccess);
+        $orderId = $results->getData()->id;
+        $results = $svc->deleteForOrderId($orderId);
+        $this->assertTrue($results->isSuccess);
+
+        // confirm deleted order is gone. Failure here, after success above, indicates likely bug in REST API itself.
+        $results = $svc->getOrder($orderId);
+        $this->assertFalse($results->isSuccess);
+
+        // also confirm attempting to get non-existent order fails.
+        $results = $svc->deleteForOrderId($orderId);
+        $this->assertFalse($results->isSuccess);
+    }
+
+    public function testDeleteProductsForOriginalOrderId()
+    {
+        $svc = new ProductAndRevenue(self::ACCOUNT_ID, self::AUTH_TOKEN);
+        $originalOrderId = $this->createTestOrder($svc);
+        $results = $svc->getOrderForOriginalOrderId($originalOrderId);
+        $this->assertTrue($results->isSuccess);
+
+        $productIds = [];
+        $products = $results->getData()->products;
+        $this->assertNotEmpty($products);
+        foreach ($products as $product) {
+            $productIds[] = $product->item_id;
+        }
+        $results = $svc->deleteProductsForOriginalOrderId($originalOrderId, $productIds);
+        $this->assertTrue($results->isSuccess);
+
+        // confirm deleted products are gone from the order. Failure here, after success above, indicates likely bug in REST API itself.
+        $results = $svc->getOrderForOriginalOrderId($originalOrderId);
+        $this->assertEmpty($results->getData()->products);
+
+        $svc->deleteForOriginalOrderId($originalOrderId);
+    }
+
+    public function testDeleteProductsForOrderId()
+    {
+        $svc = new ProductAndRevenue(self::ACCOUNT_ID, self::AUTH_TOKEN);
+        $originalOrderId = $this->createTestOrder($svc);
+        $results = $svc->getOrderForOriginalOrderId($originalOrderId);
+        $this->assertTrue($results->isSuccess);
+
+        $data = $results->getData();
+        $orderId = $data->id;
+        $productIds = [];
+        $products = $data->products;
+        $this->assertNotEmpty($products);
+        foreach ($products as $product) {
+            $productIds[] = $product->item_id;
+        }
+        $results = $svc->deleteProductsForOrderId($orderId, $productIds);
+        $this->assertTrue($results->isSuccess);
+
+        // confirm deleted products are gone from the order. Failure here, after success above, indicates likely bug in REST API itself.
+        $results = $svc->getOrderForOriginalOrderId($originalOrderId);
+        $this->assertEmpty($results->getData()->products);
+
+        $svc->deleteForOriginalOrderId($originalOrderId);
+    }
 }
